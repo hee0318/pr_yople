982c3dd3c937b6bfc03c18612fd4ff49b68e4fd3
hee0318 <hahihu12@naver.com>
2024-06-14 18:06:08 +0900

    컴포넌트분류

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,21 @@
+import { Route, Routes } from "react-router-dom";
+import Header from "./components/layout/Header";
+import Home from "./page/Home";
+import Footer from "./components/layout/Footer";
+import Main from "./components/layout/Main";
+
 function App() {
-  return <div className="App">yople</div>;
+  return (
+    <>
+      <Header />
+      <Main>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+        </Routes>
+      </Main>
+      <Footer />
+    </>
+  );
 }
 
 export default App;
